778f4f5de49cc6f8f6771cd2aa3eccbb625b1b85
Damien1223 <80947510+Damien1223@users.noreply.github.com>
2021-04-08 09:59:38 +0200

    Update TD_GitHub.cpp

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -24,6 +24,83 @@ int main ()
 
 
 
+int exposant(int nombre, int exposant)
+{
+    int nombre,exposant,calcul;
+    
+cout <<"Donner un nombre"<< endl;
+cin>> nombre;
+cout <<"Donner un exposant"<< endl;
+cin>> exposant;
+
+calcul=pow(nombre,exposant);
+
+
+return calcul;
+
+}
+
+
+
+
+
+
+float TVA(int prix)
+{
+    float prix,prixfin;
+    
+cout <<"Entrer un prix"<< endl;
+cin>> prix;
+prixfin=prix*1.196;
+return prixfin;
+
+}
+
+
+
+
+
+
+int plusGrand(int a, int b, int c
+{
+   {
+  int a,b,c;
+  
+  cout<<("Rentrez 3 nombres")<< endl;
+  cin>>a;
+  cin>>b;
+  cin>>c;
+  
+  if (a>b and a>c)
+  {
+      
+      return a;
+  }
+  
+  else if (b>a and b>c)
+  {
+     
+      return b;
+  }
+  
+  else if (c>a and c>a)
+  {
+      
+      return c;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
